6e3a22e8cfded568b069d4a16024f203cbacf99b
yeowool1010 <qyoong3579@gmail.com>
2022-12-14 05:41:44 +0900

    ✨ [Feat] 로그인 기능 구현 완료

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,35 +1,74 @@
-import React, { useState } from "react";
+import { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_URL;
+
 export const Login = () => {
-  const [userId, setUserId] = useState<string>("");
-  const [userPw, setUserPw] = useState<string>("");
   const navigate = useNavigate();
 
-  const onSubmit = async (e: any) => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [emailMessage, setEmailMessage] = useState<string>("");
+  const [passwordMessage, setPasswordMessage] = useState<string>("");
+
+  const [isEmail, setIsEmail] = useState<boolean>(false);
+  const [isPassword, setIsPassword] = useState<boolean>(false);
+
+  const onChangePassword = useCallback((e: any) => {
+    const passwordRegex = /^().{8,50}$/;
+    const passwordCurrent = e.target.value;
+    setPassword(passwordCurrent);
+
+    if (!passwordRegex.test(passwordCurrent)) {
+      setPasswordMessage("8자리 이상으로 입력 해 주세요");
+      setIsPassword(false);
+    } else {
+      setPasswordMessage("");
+      setIsPassword(true);
+    }
+  }, []);
+
+  const onChangeEmailCheck = useCallback((e: any) => {
+    const idRegex = /@/;
+
+    const emailCurrent = e.target.value;
+    setEmail(emailCurrent);
+
+    if (!idRegex.test(emailCurrent)) {
+      setEmailMessage("메일 형식에 맞춰 작성 해 주세요!");
+      setIsEmail(false);
+    } else {
+      setEmailMessage("올바른 이메일 형식이에요 : )");
+      setIsEmail(true);
+    }
+  }, []);
+
+  const onLoginSubmit = async (e: any) => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        `/user/login`,
-        JSON.stringify({ userId, userPw }),
+        baseURL + "/auth/signin",
         {
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
+          email: email,
+          password: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
       );
+      const { data, status } = response;
 
-      const { data: token, status } = response;
       if (status === 200) {
-        localStorage.setItem("token", token);
-        localStorage.setItem("userId", userId);
-        setUserId("");
-        setUserPw("");
-        // setIsLogin(true);
-        alert("로그인 성공");
-        navigate("/");
-      } else {
-        alert("아이디 혹은 비밀번호를 다시 확인 해주세요 :)");
+        localStorage.setItem("token", data.access_token);
+        localStorage.setItem("user", email);
+        setEmail("");
+        setPassword("");
+        alert("로그인 성공!");
+        navigate("/todos");
       }
     } catch (err) {
       console.error(e);
@@ -41,37 +80,43 @@ export const Login = () => {
     <>
       <div className="logo"></div>
       <div className="login-box">
-        <form onSubmit={onSubmit}>
-          <div className="input-box">
-            <p>ID</p>
+        <form onSubmit={onLoginSubmit}>
+          <div className="">
+            <p>E-mail</p>
             <input
-              value={userId}
-              onChange={(e) => {
-                setUserId(e.target.value);
-              }}
+              value={email}
+              onChange={onChangeEmailCheck}
               className="input"
               type="text"
-              placeholder="ID"
+              placeholder="E-mail"
             ></input>
+            {email.length > 0 && (
+              <span className={`message ${isEmail ? "success" : "error"}`}>
+                {emailMessage}
+              </span>
+            )}
           </div>
           <div className="input-box">
             <p>Password</p>
             <input
-              value={userPw}
-              onChange={(e) => {
-                setUserPw(e.target.value);
-              }}
+              value={password}
+              onChange={onChangePassword}
               className="input"
               type="password"
               placeholder="password"
             ></input>
+            {password.length > 0 && (
+              <span className={`message ${isPassword ? "success" : "error"}`}>
+                {passwordMessage}
+              </span>
+            )}
           </div>
           <button className="login-btn">Log in</button>
         </form>
       </div>
       <div className="text">
         <p>{`Don't have an account?`}</p>
-        <a href="/">Sign up</a>
+        <a href="/singup">Sign up</a>
       </div>
     </>
   );
